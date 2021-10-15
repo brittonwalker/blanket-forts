7ce829d93fce3ff26562beefaf9940d878ac01e3
Britton Walker <bwalker1801@gmail.com>
2021-10-14 22:02:24 -0400

    Build and version

--- wp-content/themes/blanket_forts/build/index.asset.php
+++ wp-content/themes/blanket_forts/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-polyfill'), 'version' => 'abe8b5f6a2a593aae1e55ba4279f8fbf');
+<?php return array('dependencies' => array('wp-polyfill'), 'version' => '3b8fccb570521d0adeff578f1610c147');
